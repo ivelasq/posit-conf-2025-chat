2038508ea12174d70289136fc8928d6c4e975dc3
parmsam <parmartsam@gmail.com>
2025-09-11 23:58:10 -0400

    update knowledge store

--- scripts/01-ragnar-store.R
+++ scripts/01-ragnar-store.R
@@ -28,7 +28,8 @@ store_location <- file.path("data", "posit-conf-2025.ragnar.duckdb")
 store <- ragnar::ragnar_store_create(
   location = store_location,
   embed = \(x) ragnar::embed_openai(x, model = "text-embedding-3-small"),
-  overwrite = TRUE
+  overwrite = TRUE,
+  version = 1
 )
 
 ragnar::ragnar_store_insert(store, chunks_df)
